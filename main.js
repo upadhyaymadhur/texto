99a6d947afc3b3a65a0f260e6bc47356abf9504c
upadhyaymadhur <100946494+upadhyaymadhur@users.noreply.github.com>
2022-12-12 16:41:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex=0;
+nosey=0;
+diff=0;
+right=0;
+left=0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -11,6 +16,11 @@ function setup() {
 }
 function draw(){
     background('#808080');
+    textSize(diff);
+text('M', nosex,nosey);
+fill(0, 102, 153);
+document.getElementById("side").innerHTML="Width and hight of text is = "+diff+" px";
+
 }
 function modelLoaded(){
     console.log('PoseNet Is Initialized');
@@ -19,5 +29,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        right=results[0].pose.rightWrist.x;
+        left=results[0].pose.leftWrist.x;
+        console.log("nosex = "+nosex+"nosey = "+nosey);
+        diff=floor(left-right);
+        console.log("left = "+left+"right = "+right+"diff = "+diff);
+
+
+
     }
 }
